d7df58360730b7362320580a463eb0c01cd1f8b0
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-22 15:39:39 +0545

    get recent tweets

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ except:
 
 for tweet in tweepy.Cursor(
     api.search_tweets,
-    "#osm OR #openstreetmap OR #OSM OR #OPENSTREETMAP OR #HOTOSM OR #hotosm exclude:retweets",
+    "#osm OR #openstreetmap OR #OSM OR #OPENSTREETMAP OR #HOTOSM OR #hotosm filter:nativeretweets exclude:retweets",
     count=300,
+    result_type="recent",
     tweet_mode="extended",
 ).items():
     count = 0
