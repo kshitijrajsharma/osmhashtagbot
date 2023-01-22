485f9457c7fb771972c1caee39837011d1bd3ff2
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-22 16:15:42 +0545

    remove sleep option

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ for tweet in tweepy.Cursor(
         api.retweet(tweet.id)
         print(f"Retweeted tweet by {tweet.user.screen_name}")
         count = count + 1
-        time.sleep(2)
+        # time.sleep(2)
     except Exception as e:
         print(e)
 print(f"Retweeted {count} tweets")
