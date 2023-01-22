b3fdf3e2c499d85ff0c2da4725bd4aba07155458
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-22 16:14:17 +0545

    check

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ auth = tweepy.OAuthHandler(os.environ["API_KEY"], os.environ["API_KEY_SECRET"])
 auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
-print(api.rate_limit_status()["resources"])
+print(api.rate_limit_status())
 try:
     api.verify_credentials()
     print("Authentication OK")
