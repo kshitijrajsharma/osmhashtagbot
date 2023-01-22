44d0000a5b5133e2e0d1c68f19197e886c127ef7
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-22 16:02:22 +0545

    reverted all change

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import tweepy
 
@@ -7,10 +6,7 @@ import tweepy
 auth = tweepy.OAuthHandler(os.environ["API_KEY"], os.environ["API_KEY_SECRET"])
 auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-# Get rate limit status
-rate_limit_status = api.rate_limit_status()
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 try:
     api.verify_credentials()
@@ -18,30 +14,16 @@ try:
 except:
     print("Error during authentication")
 
-count = 0
-# Search for tweets
-tweets = tweepy.Cursor(
-    api.search_tweets,
-    q="#openstreetmap #hotosm -filter:retweets",
-    count=100,
-    tweet_mode="extended",
-).items()
-print(tweets)
 
-# Retweet the tweets
-for tweet in tweets:
-    print(tweet)
-    if (
-        api.rate_limit_status()["resources"]["statuses"]["/statuses/retweet/:id"][
-            "remaining"
-        ]
-        == 0
-    ):
-        print("I have reached the rate limit for this endpoint.")
-        break
-    else:
+for tweet in tweepy.Cursor(
+    api.search_tweets,
+    "#osm OR #openstreetmap OR #OSM OR #OPENSTREETMAP OR #HOTOSM OR #hotosm -filter:retweets",
+    count=300,
+).items():
+    count = 0
+    try:
         api.retweet(tweet.id)
         count = count + 1
-        print(f"Retweeted tweet by {tweet.user.screen_name}")
-
+    except Exception as e:
+        print(e)
 print(f"Retweeted {count} tweets")
